47afa3df4b05dff1f02f63d69a92ed887ce23601
Grant Overby <grant.overby@gmail.com>
2025-09-05 19:23:22 -0500

    user configurable delay

--- EqSmartFire.py
+++ EqSmartFire.py
@@ -4,29 +4,55 @@ import win32gui
 import keyboard
 
 
-PRESS_DELAY = 100 # milliseconds
-LOOP_DELAY = 1000 # milliseconds
-
-
-USAGE_INFO = '''
-CTRL + ALT + hotkey     : Toggle spam for corresponding hotkey
-CTRL + ALT + [          : Pause spamming
-CTRL + ALT + ]          : Resume spamming
-/                       : Enter typing mode
-r                       : Enter typing mode
-ENTER                   : Exit typing mode
-CTRL + ALT + /          : Exit typing mode
-CTRL + ALT + `          : Reset all settings
+print('''
+CTRL + ALT + hotkey        : Toggle spam for corresponding hotkey
+CTRL + ALT + [             : Pause spamming
+CTRL + ALT + ]             : Resume spamming
+/                          : Enter typing mode
+r                          : Enter typing mode
+ENTER                      : Exit typing mode
+CTRL + ALT + /             : Exit typing mode
+CTRL + ALT + SHIFT + delay : Sets delay between key presses
+CTRL + ALT + `             : Reset all settings
 
 Spamming is suppressed if paused, typing, a modifier key is held, or if EverQuest is not the foreground window.
 Multiple hotkeys can be toggled simultaneously.
-Valid hotkeys are: 1 2 3 4 5 6 7 8 9 0 - =
-'''
+Available hotkeys: 1 2 3 4 5 6 7 8 9 0 - =''')
+
+
+DEFAULT_DELAY = '2'
+DELAYS = {
+    '1': 0.25,
+    '2': 0.5,
+    '3': 0.75,
+    '4': 1,
+    '5': 2,
+    '6': 10,
+    '7': 60,
+}
 
 
 keys = []
 typing = False
 paused = False
+delay = DELAYS.get(DEFAULT_DELAY)
+
+
+delay_descriptions = ''
+for key, val in DELAYS.items():
+    delay_descriptions += f'{key}: {val}s, '
+delay_descriptions = delay_descriptions[:-2]
+print(f'Available delays: {delay_descriptions}')
+print(f'Default delay: {DEFAULT_DELAY}: {DELAYS.get(DEFAULT_DELAY)}s')
+
+
+def set_delay(val):
+    global delay
+    global DELAYS
+    delay = DELAYS.get(val)
+
+for key in DELAYS:
+    keyboard.add_hotkey('ctrl+alt+shift+' + key, set_delay, args=(key,))
 
 
 def toggle_key(val):
@@ -62,14 +88,13 @@ def reset():
     global keys
     global typing
     global paused
+    global DEFAULT_DELAY
     keys.clear()
     typing = False
     paused = False
+    set_delay(DEFAULT_DELAY)
 
 keyboard.add_hotkey('ctrl+alt+`', reset)
-
-
-print(USAGE_INFO)
 
 
 def is_active():
@@ -88,10 +113,10 @@ def is_active():
 
 try:
     while True:
-        time.sleep(LOOP_DELAY / 1000)
+        time.sleep(delay)
         for key in keys:
             if is_active():
                 pydirectinput.press(key)
-                time.sleep(PRESS_DELAY / 1000)
+                time.sleep(delay / 10)
 except KeyboardInterrupt:
     pass
--- README.md
+++ README.md
@@ -1,19 +1,24 @@
 EqSmartFire spams hotkeys in EverQuest. It was inspired by AutoFire and aims to improve upon it by reducing 
 interference with attended game play.
 
-| Shortcut            | Function                             |
-|---------------------|--------------------------------------|
-| CTRL + ALT + hotkey | Toggle spam for corresponding hotkey |
-| CTRL + ALT + [      | Pause spamming                       |
-| CTRL + ALT + ]      | Resume spamming                      |
-| /                   | Enter typing mode                    |
-| r                   | Enter typing mode                    |
-| ENTER               | Exit typing mode                     |
-| CTRL + ALT + /      | Exit typing mode                     |
-| CTRL + ALT + `      | Reset all settings                   |
+| Shortcut                   | Function                             |
+|----------------------------|--------------------------------------|
+| CTRL + ALT + hotkey        | Toggle spam for corresponding hotkey |
+| CTRL + ALT + [             | Pause spamming                       |
+| CTRL + ALT + ]             | Resume spamming                      |
+| /                          | Enter typing mode                    |
+| r                          | Enter typing mode                    |
+| ENTER                      | Exit typing mode                     |
+| CTRL + ALT + /             | Exit typing mode                     |
+| CTRL + ALT + SHIFT + delay | Sets delay between key presses       |
+| CTRL + ALT + `             | Reset all settings                   |
 
 Spamming is suppressed if paused, typing, a modifier key is held, or if EverQuest is not the foreground window.
 
 Multiple hotkeys can be toggled simultaneously.
 
-Valid hotkeys are: 1 2 3 4 5 6 7 8 9 0 - =
+Available hotkeys: 1 2 3 4 5 6 7 8 9 0 - =
+
+Available delays: 1: 0.25s, 2: 0.5s, 3: 0.75s, 4: 1s, 5: 2s, 6: 10s, 7: 60s
+
+Default delay: 2: 0.5s
